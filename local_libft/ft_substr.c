b866bf08356f7e3af446d156cdc8a74d5753cf72
Isxfiane <sofianebenrhouma93100@gmail.com>
2023-11-12 15:41:13 +0100

    test

--- local_libft/ft_substr.c
+++ local_libft/ft_substr.c
@@ -12,30 +12,37 @@
 
 #include "libft.h"
 
+static char	*error(void)
+{
+	char	*error;
+	error = (char *) malloc (sizeof(char) * 1);
+	if (error == NULL)
+		return (NULL);
+	error[0] = '\0';
+	return (error);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*cpy;
 	unsigned int	i;
 	unsigned int	k;
 	unsigned int	count;
-	char			*error;
 
 	i = start;
-	k = -1;
-	count = -1;
-	error = (char *) malloc (sizeof(char) * 1);
-	error[0] = '\0';
-	if (error == NULL)
-		return (NULL);
+	k = 0;
+	count = 0;
 	if (start > ft_strlen(s) + 1 || s[0] == '\0')
-		return (error);
+		return (error());
 	cpy = (char *) malloc (sizeof(char) * (len + 1));
 	if (cpy == NULL)
 		return (NULL);
-	while ((s[i] != '\0') && (++count < len))
+	while ((s[i] != '\0') && (count < len))
 	{
-		cpy[++k] = s[i];
+		cpy[k] = s[i];
 		i++;
+		k++;
+		count++;
 	}
 	cpy[k] = '\0';
 	return (cpy);
